82a6985d58ace22d9558b1dcd117261c2c610afd
Utiyama <utiyama@cet-app.com>
2024-03-07 22:16:39 +0700

    Collect detail sql

--- database/init.sql
+++ database/init.sql
@@ -12,16 +12,21 @@ CREATE TABLE  LP_ADMIN (
 
 INSERT INTO LP_ADMIN
 (email, phone, password, username, fullname)
-VALUES( 'eamil', 'phone', 'password', 'username', 'fullname');
+VALUES( 'email', 'phone', 'password', 'username', 'fullname');
 
 CREATE TABLE LP_USER (
     id VARCHAR(255) NOT NULL PRIMARY KEY DEFAULT (uuid()),
     contact_id VARCHAR(255) NOT NULL,
+    prefecture_id VARCHAR(225) NOT NULL,
     email VARCHAR(255),
     phone VARCHAR(225),
     password VARCHAR(225),
     username VARCHAR(225),
     fullname VARCHAR(225),
+    name_kanji VARCHAR(225),
+    name_kana VARCHAR(225),
+    birthday VARCHAR(225),
+    address VARCHAR(225),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP,
@@ -32,8 +37,14 @@ CREATE TABLE LP_USER (
 CREATE TABLE LP_STORE (
     id VARCHAR(255) NOT NULL PRIMARY KEY DEFAULT (uuid()),
     contact_id VARCHAR(225) NOT NULL,
+    prefecture_id VARCHAR(225) NOT NULL,
     store_key VARCHAR(255) unique,
     store_name VARCHAR(255),
+    store_name_kana VARCHAR(255),
+    owner VARCHAR(255),
+    zip_code VARCHAR(255),
+    phone VARCHAR(255),
+    reject_reason VARCHAR(255),
     status VARCHAR(255),
     remark VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -46,12 +57,15 @@ CREATE TABLE LP_STORE (
 CREATE TABLE LP_SELLER (
     id VARCHAR(255) NOT NULL PRIMARY KEY,
     contact_id VARCHAR(225) NOT NULL,
+    prefecture_id VARCHAR(225),
     store_id VARCHAR(255),
     email VARCHAR(255),
     phone VARCHAR(225),
     password VARCHAR(225),
-    username VARCHAR(225),
-    fullname VARCHAR(225),
+    office_name  VARCHAR(225),
+    office_name_kana  VARCHAR(225),
+    post_code VARCHAR(225),
+    address VARCHAR(225),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP,
