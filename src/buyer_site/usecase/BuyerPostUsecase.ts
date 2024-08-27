627f2e0a1f8f3d7c7674d7d2a69d73276882180b
Vu Tran <vutran260@gmail.com>
2024-08-27 19:48:04 +0700

    328 add filter store

--- src/buyer_site/usecase/BuyerPostUsecase.ts
+++ src/buyer_site/usecase/BuyerPostUsecase.ts
@@ -1,25 +1,24 @@
-import { Filter, Paging } from "../../lib/paging/Request";
-import { BuyerPostRepository } from "../repository/BuyerPostRepository";
-import { LpOrder } from "../../lib/paging/Order";
+import { Filter, Paging } from '../../lib/paging/Request';
+import { BuyerPostRepository } from '../repository/BuyerPostRepository';
+import { LpOrder } from '../../lib/paging/Order';
 
 export class BuyerPostUsecase {
-    private buyerPostRepo: BuyerPostRepository
-    constructor(buyerPostRepo: BuyerPostRepository) {
-        this.buyerPostRepo = buyerPostRepo
-    }
+  private buyerPostRepo: BuyerPostRepository;
+  constructor(buyerPostRepo: BuyerPostRepository) {
+    this.buyerPostRepo = buyerPostRepo;
+  }
 
-    public getPosts = async (
-        filter: Filter[],
-        order: LpOrder[],
-        paging: Paging,
-    ) => {
-        return await this.buyerPostRepo.getPosts(paging, order, filter)
-    }
-    public getPost = async (id: string,) => {
-        const post = await this.buyerPostRepo.getPostById(id)
-        return post
-    }
+  public getPosts = async (
+    filter: Filter[],
+    order: LpOrder[],
+    paging: Paging,
+  ) => {
+    console.log('filter', filter);
+    return await this.buyerPostRepo.getPosts(paging, order, filter);
+  };
 
-
-
+  public getPost = async (id: string) => {
+    const post = await this.buyerPostRepo.getPostById(id);
+    return post;
+  };
 }
