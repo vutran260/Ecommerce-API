d54766b1302f9ee38bcb3ff1de02f5402a39b649
Vu Tran <vutran260@gmail.com>
2024-09-08 16:44:06 +0700

    96 sso buyer

---
+++ src/buyer_site/endpoint/SSOEndpoint.ts
@@ -0,0 +1,38 @@
+import express, { Response } from 'express';
+
+import { ProtectedRequest } from '../../lib/http/app-request';
+import { ResponseData } from '../../lib/http/Response';
+import { BadRequestError } from '../../lib/http/custom_error/ApiError';
+import { SSOUseCase } from '../usecase/SSOUseCase';
+
+export class SSOEndpoint {
+  private ssoUseCase: SSOUseCase;
+
+  constructor(ssoUseCase: SSOUseCase) {
+    this.ssoUseCase = ssoUseCase;
+  }
+
+  private registerSSOUser = async (req: ProtectedRequest, res: Response) => {
+    if (!req.body.accessToken) {
+      throw new BadRequestError('accessToken is require');
+    }
+    if (!req.body.userAlias) {
+      throw new BadRequestError('userAlias is require');
+    }
+    if (!req.body.contractId) {
+      throw new BadRequestError('contractId is require');
+    }
+    const result = await this.ssoUseCase.registerSSOUser(
+      req.body.accessToken,
+      req.body.userAlias,
+      req.body.contractId,
+    );
+    return ResponseData(result, res);
+  };
+
+  public getRouter() {
+    const router = express.Router();
+    router.post('/register-user', this.registerSSOUser);
+    return router;
+  }
+}
